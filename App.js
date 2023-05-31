cfd84f50014600af04110eee31e840b3a66219f2
Patrick Lapke <plapke@zagmail.gonzaga.edu>
2023-05-31 11:23:20 +0100

    Added first components.

--- App.js
+++ App.js
@@ -1,20 +1,6 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import AppButton from "./app/components/AppButton";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <AppButton></AppButton>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ app/components/AppButton.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Text, TouchableOpacity, StyleSheet } from "react-native";
+
+function AppButton(props) {
+  return (
+    <TouchableOpacity>
+      <Text></Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {},
+  text: {},
+});
+
+export default AppButton;
