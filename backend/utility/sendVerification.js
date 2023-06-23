40b968b79afa8e9552fd4134643d6f85331e0e6f
Patrick Lapke <plapke@zagmail.gonzaga.edu>
2023-06-23 15:01:16 +0100

    Cleaning files.

--- backend/utility/sendVerification.js
+++ backend/utility/sendVerification.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const sgMail = require("@sendgrid/mail");
+const { IP_HOME, IP_SCHOOL } = require("../../app/IP/ip");
 
 const API_KEY = process.env.SENDGRID_API;
 
@@ -15,8 +16,8 @@ async function sendVerification(email, token) {
       email: "FallbrookTennisAndPickleball@gmail.com",
     },
     subject: "Verify your email address",
-    text: `Please verify your email by clicking on the following link: \nhttp://192.168.0.16:3000/api/verify/${token}`,
-    html: `<p>Please verify your email by clicking on the following link: <a href="http://192.168.0.16:3000/api/verify/${token}">Verify Email</a></p>`,
+    text: `Please verify your email by clicking on the following link: \nhttp://${IP_HOME}:3000/api/verify/${token}`,
+    html: `<p>Please verify your email by clicking on the following link: <a href="http://${IP_HOME}:3000/api/verify/${token}">Verify Email</a></p>`,
   };
 
   await sgMail.send(msg);
